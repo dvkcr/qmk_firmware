8f0d8783cdb99d2f4568bc7c26a8ff8f6c017c33
Dave Creech <dkcreech@gmail.com>
2024-03-23 19:18:26 -0500

    Working, tested bnadit_hat

--- keyboards/handwired/bandit_hat/keymaps/default/keymap.c
+++ keyboards/handwired/bandit_hat/keymaps/default/keymap.c
@@ -35,17 +35,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
  */
     [_COLEMAK_L] = LAYOUT_split_3x5_4
     (
-    /* left hand */
-        KC_Q,           KC_W,           KC_F,                   KC_P,           KC_G,
-        LGUI_T(KC_A),   LALT_T(KC_R),   LCTL_T(KC_S),           LSFT_T(KC_T),   KC_D,
-        KC_Z,           KC_X,           KC_C,                   LT(_NAV, KC_V), KC_B,
-                        KC_MEH,         LT(_LOWER, KC_TAB),     KC_ENT,         LCTL(KC_B),
-    /* right hand */
-         KC_J,          KC_L,           KC_U,                   KC_Y,           KC_SCLN,
-         KC_H,          RSFT_T(KC_N),   RCTL_T(KC_E),           RALT_T(KC_I),   RGUI_T(KC_O),
-         KC_K,          KC_M,           KC_COMM,                KC_DOT,         KC_SLSH,
-         RCTL(KC_B),    KC_SPC,         LT(_RAISE, KC_BSPC),    KC_HYPR
-     ),
+/* L */ KC_Q,         KC_W,         KC_F,                KC_P,           KC_G,
+/* R */ KC_J,         KC_L,         KC_U,                KC_Y,           KC_SCLN,
+/* L */ LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S),        LSFT_T(KC_T),   KC_D,
+/* R */ KC_H,         RSFT_T(KC_N), RCTL_T(KC_E),        RALT_T(KC_I),   RGUI_T(KC_O),
+/* L */ KC_Z,         KC_X,         KC_C,                LT(_NAV, KC_V), KC_B,
+/* R */ KC_K,         KC_M,         KC_COMM,             KC_DOT,         KC_SLSH,
+/* L */               KC_MEH,       LT(_LOWER, KC_TAB),  KC_ENT,         LCTL(KC_B),
+/* R */ RCTL(KC_B),   KC_SPC,       LT(_RAISE, KC_BSPC), KC_HYPR
+    ),
 
 /*  COLEMAK_R (CAGS)
  *                ,------.                                                  ,------.
@@ -63,17 +61,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
  */
     [_COLEMAK_M] = LAYOUT_split_3x5_4
     (
-    /* left hand */
-        KC_Q,           KC_W,           KC_F,                   KC_P,           KC_G,
-        LCTL_T(KC_A),   LALT_T(KC_R),   LGUI_T(KC_S),           LSFT_T(KC_T),   KC_D,
-        KC_Z,           KC_X,           KC_C,                   LT(_NAV, KC_V), KC_B,
-                        KC_MEH,         LT(_LOWER, KC_TAB),     KC_ENT,         LCTL(KC_B),
-    /* right hand */
-        KC_J,           KC_L,           KC_U,                   KC_Y,           KC_SCLN,
-        KC_H,           RSFT_T(KC_N),   RGUI_T(KC_E),           RALT_T(KC_I),   RCTL_T(KC_O),
-        KC_K,           KC_M,           KC_COMM,                KC_DOT,         KC_SLSH,
-        RCTL(KC_B),     KC_SPC,         LT(_RAISE, KC_BSPC),    KC_HYPR
-     ),
+/* L */ KC_Q,           KC_W,         KC_F,                KC_P,           KC_G,
+/* R */ KC_J,           KC_L,         KC_U,                KC_Y,           KC_SCLN,
+/* L */ LCTL_T(KC_A),   LALT_T(KC_R), LGUI_T(KC_S),        LSFT_T(KC_T),   KC_D,
+/* R */ KC_H,           RSFT_T(KC_N), RGUI_T(KC_E),        RALT_T(KC_I),   RCTL_T(KC_O),
+/* L */ KC_Z,           KC_X,         KC_C,                LT(_NAV, KC_V), KC_B,
+/* R */ KC_K,           KC_M,         KC_COMM,             KC_DOT,         KC_SLSH,
+/* L */                 KC_MEH,       LT(_LOWER, KC_TAB),  KC_ENT,         LCTL(KC_B),
+/* R */ RCTL(KC_B),     KC_SPC,       LT(_RAISE, KC_BSPC), KC_HYPR
+    ),
 
 /*  _NAV
  *                ,------.                                                  ,------.
@@ -91,17 +87,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
  */
     [_NAV] = LAYOUT_split_3x5_4
     (
-    /* left hand */
-        QK_BOOT,        _______,        _______,            _______,        _______,
-        _______,        _______,        _______,            _______,        _______,
-        _______,        _______,        _______,            _______,        _______,
-                        _______,        _______,            KC_ESC,         COLEMAK_L,
-    /* right hand */
-        _______,        _______,        KC_UP,              KC_DEL,         KC_BSPC,
-        _______,        KC_LEFT,        KC_DOWN,            KC_RGHT,        KC_ENT,
-        _______,        KC_PGUP,        KC_PGDN,            KC_HOME,        KC_END,
-        COLEMAK_M,      KC_ESC,         _______,            _______
-     ),
+/* L */ QK_BOOT,        _______,        _______,            _______,        _______,
+/* R */ _______,        _______,        KC_UP,              KC_DEL,         KC_BSPC,
+/* L */ _______,        _______,        _______,            _______,        _______,
+/* R */ _______,        KC_LEFT,        KC_DOWN,            KC_RGHT,        KC_ENT,
+/* L */ _______,        _______,        _______,            _______,        _______,
+/* R */ _______,        KC_PGUP,        KC_PGDN,            KC_HOME,        KC_END,
+/* L */                 _______,        _______,            KC_ESC,         COLEMAK_L,
+/* R */ COLEMAK_M,      KC_ESC,         _______,            _______
+    ),
 
 /*  _LOWER
  *                ,------.                                                  ,------.
@@ -120,15 +114,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     [_LOWER] = LAYOUT_split_3x5_4
     (
     /* left hand */
-        KC_1,           KC_2,           KC_3,               KC_4,           KC_5,
-        KC_F1,          KC_F2,          KC_F3,              KC_F4,          KC_F5,
-        KC_F7,          KC_F8,          KC_F9,              KC_F10,         KC_F11,
-                        _______,        _______,            _______,        _______,
-    /* right hand */
-        KC_6,           KC_7,           KC_8,               KC_9,           KC_0,
-        KC_F6,          KC_MINS,        KC_EQL,             KC_LBRC,        KC_RBRC,
-        KC_F12,         KC_QUOT,        KC_BSLS,            KC_GRV,         _______,
-        _______,        _______,        _______,            _______
+/* L */ KC_1,           KC_2,           KC_3,               KC_4,           KC_5,
+/* R */ KC_6,           KC_7,           KC_8,               KC_9,           KC_0,
+/* L */ KC_F1,          KC_F2,          KC_F3,              KC_F4,          KC_F5,
+/* R */ KC_F6,          KC_MINS,        KC_EQL,             KC_LBRC,        KC_RBRC,
+/* L */ KC_F7,          KC_F8,          KC_F9,              KC_F10,         KC_F11,
+/* R */ KC_F12,         KC_QUOT,        KC_BSLS,            KC_GRV,         _______,
+/* L */                 _______,        _______,            _______,        _______,
+/* R */ _______,        _______,        _______,            _______
      ),
 
 /*  _RAISE
@@ -147,16 +140,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
  */
     [_RAISE] = LAYOUT_split_3x5_4
     (
-    /* left hand */
-        KC_EXLM,        KC_AT,          KC_HASH,            KC_DLR,         KC_PERC,
-        KC_F1,          KC_F2,          KC_F3,              KC_F4,          KC_F5,
-        KC_F7,          KC_F8,          KC_F9,              KC_F10,         KC_F11,
-                        _______,        _______,            _______,        _______,
-    /* right hand */
-        KC_CIRC,        KC_AMPR,        KC_ASTR,            KC_LPRN,        KC_RPRN,
-        KC_F6,          KC_UNDS,        KC_PLUS,            KC_LCBR,        KC_RCBR,
-        KC_F12,         KC_DQUO,        KC_PIPE,            KC_TILD,        _______,
-        _______,        _______,        _______,            _______
+/* L */ KC_EXLM,        KC_AT,          KC_HASH,            KC_DLR,         KC_PERC,
+/* R */ KC_CIRC,        KC_AMPR,        KC_ASTR,            KC_LPRN,        KC_RPRN,
+/* R */ KC_F1,          KC_F2,          KC_F3,              KC_F4,          KC_F5,
+/* L */ KC_F6,          KC_UNDS,        KC_PLUS,            KC_LCBR,        KC_RCBR,
+/* R */ KC_F7,          KC_F8,          KC_F9,              KC_F10,         KC_F11,
+/* L */ KC_F12,         KC_DQUO,        KC_PIPE,            KC_TILD,        _______,
+/* R */                 _______,        _______,            _______,        _______,
+/* L */ _______,        _______,        _______,            _______
      ),
 };
 
